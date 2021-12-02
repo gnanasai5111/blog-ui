30db32e2f7decb95efa62a1eb5a53d6d5739559c
gnanasai <gnanasai5111@gmail.com>
2021-12-02 20:01:48 +0530

    deploy

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,58 @@
+import { Link } from "react-router-dom";
+import "./topbar.css";
+
+export default function Topbar() {
+  const user = true;
+  return (
+    <div className="top">
+      <div className="topLeft">
+        <i className="topIcon fab fa-facebook"></i>
+        <i className="topIcon fab fa-instagram"></i>
+        <i className="topIcon fab fa-pinterest"></i>
+        <i className="topIcon fab fa-twitter"></i>
+      </div>
+      <div className="topCenter">
+        <ul className="topList">
+          <li className="topListItem">
+            <Link className="link" to="/">
+              HOME
+            </Link>
+          </li>
+          {/* <li className="topListItem">ABOUT</li>
+          <li className="topListItem">CONTACT</li> */}
+          <li className="topListItem">
+            <Link className="link" to="/write">
+              WRITE
+            </Link>
+          </li>
+          {user && <li className="topListItem">LOGOUT</li>}
+        </ul>
+      </div>
+      <div className="topRight">
+        {user ? (
+          <Link className="link" to="/settings">
+            <img
+              className="topImg"
+              src="./assets/images/gnanasai.jpg"
+              alt=""
+            />
+          </Link>
+        ) : (
+          <ul className="topList">
+            <li className="topListItem">
+              <Link className="link" to="/login">
+                LOGIN
+              </Link>
+            </li>
+            <li className="topListItem">
+              <Link className="link" to="/register">
+                REGISTER
+              </Link>
+            </li>
+          </ul>
+        )}
+        <i className="topSearchIcon fas fa-search"></i>
+      </div>
+    </div>
+  );
+}
